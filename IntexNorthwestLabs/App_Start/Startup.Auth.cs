222892b161e7d67215464144b681c2790f22d2fd
Luke DeBaltzo <lukedebaltzo@gmail.com>
2019-12-04 12:16:57 -0700

    hopefully this doesn't mess anything up. lol

---
+++ IntexNorthwestLabs/App_Start/Startup.Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+[assembly: OwinStartupAttribute(typeof(IntexNorthwestLabs.Startup))]
+
+namespace IntexNorthwestLabs
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
