3eabbd74f74e559143a573fa55a2a0a70dac8127
Luke DeBaltzo <lukedebaltzo@gmail.com>
2019-12-04 19:33:53 -0700

    hopefully making assay_catalog

---
+++ IntexNorthwestLabs/Controllers/Assay_CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using IntexNorthwestLabs.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntexNorthwestLabs.Models;
+
+
+namespace IntexNorthwestLabs.Controllers
+{
+    public class Assay_CatalogController : Controller
+    {
+        private NorthWestLabsContext db = new NorthWestLabsContext();
+
+        // GET: Assay_Catalog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- IntexNorthwestLabs/Models/Assay_Catalog.cs
+++ IntexNorthwestLabs/Models/Assay_Catalog.cs
@@ -2,10 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace IntexNorthwestLabs.Models
 {
+    [Table("dbo.Assay_Catalog")]
     public class Assay_Catalog
     {
+        [Key]
+        [Required]
+        [Display(Name = "Assay Code")]
+        public int AssayCode { get; set; }
+
+        [Display(Name = "Assay Type")]
+        [StringLength(20)]
+        public string AssayType { get; set; }
+
+        [Display(Name = "Assay Name")]
+        [StringLength(50)]
+        public string AssayName { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "Assay Description")]
+        public string AssayDescription { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "Assay Literature References")]
+        public string AssayLiteratureRefs { get; set; }
+
+        [Display(Name = "Test Name")]
+        public string TestName { get; set; }
+
+        [Display(Name = "Test Description")]
+        public string TestDescription { get; set; }
+
+        [Display(Name = "Test Type")]
+        public string TestType { get; set; }
+
+        [Display(Name = "Test Duration")]
+        public int TestDuration { get; set; }
+
+        [Display(Name = "Required Test")]
+        public bool RequiredTest { get; set; }
+
+        [Column(Order = 3)]
+        [Display(Name = "Compound Name")]
+        public string CompoundName { get; set; }
+
+        [Display(Name = "Material Name")]
+        public string MaterialName { get; set; }
     }
 }
